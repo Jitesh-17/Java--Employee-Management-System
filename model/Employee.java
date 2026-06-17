5ab94ac2306399119f5a1bc73717972b7eace3f3
Jitesh-17 <jiteshdas20@gmail.com>
2026-06-17 23:01:24 +0530

    Created an Abstract class with abstract calculate salary method

---
+++ model/Employee.java
@@ -0,0 +1,35 @@
+package model;
+
+public abstract class Employee {
+	
+	private int id;
+	private String name;
+	private String department; 
+
+
+	public Employee(int id,String name,String department) {
+		this.id = id;
+		this.name = name;
+		this.department = department;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDepartment() {
+		return department;	
+	}
+	
+	public abstract double calculateSalary();
+	
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", department=" + department + "]";
+	}
+}
